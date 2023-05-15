99067ffa950b40f8f60e78bd414d73a8a85725d8
Kilas-aniso <kilasaniso@gmail.com>
2023-05-15 10:43:38 +0300

    registration

---
+++ registration.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def register(self):
+        new_user = User()
+        return new_user
+
+    def login(self):
+        # logIN with the new user
+     pass
+
+     
